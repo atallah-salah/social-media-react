8f73e7d72dde7feb87df65872f30000bd5fc6268
atallah-salah <atallah_salah@outlook.com>
2020-02-25 10:26:32 +0200

    add react-bootstrap, bootstrap to  dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// import dependencies
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+
+// import pages
+import Auth from "./pages/Auth/Auth";
+import Main from "./pages/Main/Main";
+
+// import components
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <>
+      <Navbar />
+      <Router>
+        <div>
+          <Route path="/" component={Main} exact />
+          <Route path="/auth" component={Auth} exact />
+        </div>
+      </Router>
+    </>
   );
 }
 
